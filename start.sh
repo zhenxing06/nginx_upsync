d280f9e0548c162963ae0634e4befeca3f6a6dae
zhenxing_06 <zhenxing_06@163.com>
2020-12-23 19:08:26 +0800

    add files

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /data/soft/consul
+
+nohup ./consul agent -server -ui -bootstrap-expect=1 -syslog -bind=10.10.78.17 -client=0.0.0.0  -data-dir=/data/soft/consul/data -log-level=debug &
