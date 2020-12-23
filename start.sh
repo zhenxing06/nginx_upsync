a35cb47ff27fc0a779664087e7df209aa358d758
fengmanlou <zhenxing_06@163.com>
2020-12-23 19:10:05 +0800

    Update start.sh
    change -bind=0.0.0.0

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /data/soft/consul
 
-nohup ./consul agent -server -ui -bootstrap-expect=1 -syslog -bind=10.10.78.17 -client=0.0.0.0  -data-dir=/data/soft/consul/data -log-level=debug &
+nohup ./consul agent -server -ui -bootstrap-expect=1 -syslog -bind=0.0.0.0 -client=0.0.0.0  -data-dir=/data/soft/consul/data -log-level=debug &
